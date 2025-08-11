bb51254f8b637c7992bf74c2039534e11c699f05
Moises <info.castillo01@gmail.com>
2025-08-11 16:30:18 -0400

    small changes

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {useAuthStore} from '../store/authStore';
+import {SideBar} from '../components/SideBar';
+
+export const Home = ({ tab }) => {
+  const {user} = useAuthStore();
+
+  useEffect(() => {
+    if (user) console.log(user);
+  }, [user]);
+
+  return (
+    <div className="w-full flex justify-center items-center flex-col gap-10">
+      <SideBar initialTab={tab} />
+    </div>
+  );
+}
