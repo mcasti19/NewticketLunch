2fb3c5b88d958fed69a740988a4c42ea71610012
Mcasti87 <info.castillo01@gmail.com>
2025-09-05 14:21:09 -0400

    folder cleaned

--- frontend/src/components/HeaderLogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const HeaderLogo = () => {
-    return (
-        <div className=''>
-            <img src="/logoMercal2.png" alt="Logo Mercal" width={90} height={90} className='rounded-full' />
-        </div>
-    )
-}
