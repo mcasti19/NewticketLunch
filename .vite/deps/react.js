f31ed0c2bf5e6c58350a64a2f85c38ea4b4087bb
Mcasti87 <info.castillo01@gmail.com>
2025-09-05 14:21:09 -0400

    folder cleaned

---
+++ .vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-MC2JJCLE.js";
+import "./chunk-OL46QLBJ.js";
+export default require_react();
--- frontend/src/hooks/useCheckToken.js
+++
@@ -1,6 +0,0 @@
-import {useAuthStore} from '../store/authStore';
-
-export const useCheckToken = () => {
-    const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-    return { isAuthenticated };
-};
