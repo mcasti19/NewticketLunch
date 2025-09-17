2cb02808cfc29eed2f4e7c5c68b725bf46da25bd
Mcasti87 <castdevel@gmail.com>
2025-09-17 16:52:49 -0400

    small changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,6 @@
-// import React, {useEffect} from 'react';
-// import {useAuthStore} from '../store/authStore';
 import {SideBar} from '../components/SideBar';
 
 export const Home = ({ tab }) => {
-  // const {user} = useAuthStore();
-
-  // useEffect(() => {
-  //   if (user) console.log(user);
-  // }, [user]);
 
   return (
     <div className="w-full flex justify-center items-center flex-col gap-10">
